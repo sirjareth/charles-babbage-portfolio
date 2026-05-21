82ae8256ca2c2557a71327fb1e2cf7ed4c4bc7f9
sirjareth <jarethronquillo12@gmail.com>
2026-05-22 07:20:20 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+	import Navbar from './components/Navbar.vue';
+	import About from './components/About.vue';
+	import Projects from './components/Projects.vue';
+	import Tools from	 './components/Tools.vue';
+	import Contacts from './components/Contacts.vue';
+	import FooterComponent from './components/FooterComponent.vue';
+</script>
+
+<template>
+  <Navbar/>
+  <div class="container-fluid">
+  	<About/>
+  </div>
+  
+  <Projects/>
+  <Tools/>
+  <Contacts/>
+  <FooterComponent/>
+</template>
---
+++ src/components/Projects.vue
@@ -0,0 +1,38 @@
+<template>
+	<!-- My Projects -->
+	<section class="pb-5" id="projects">
+	    <h1 class="mt-5 mb-5 pb-4 text-center">My Projects</h1>
+	    
+	    <div
+	    	v-for="(group, index) in chunkedProjects"
+	    	:key="index"
+	    	class="card-deck my-5 justify-content-center"
+	    >
+	    	<ProjectCard
+	    		v-for="project in group"
+	    		:key="project.id"
+	    		:project="project"
+	    	/>
+	    </div>
+	</section>
+</template>
+
+<script setup>
+	import { computed } from 'vue';
+
+	import ProjectCard from './ProjectCard.vue';
+
+	import projects from '../data/projects.json';
+
+	const chunckSize = 3;
+
+	const chunkedProjects = computed(() => {
+		const chuncks = [];
+
+		for (let i = 0; i < projects.length; i += chunckSize){
+			chuncks.push(projects.slice(i, i + chunckSize));
+		}
+
+		return chuncks;
+	})
+</script>
